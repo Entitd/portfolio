5a8ed9dd30c7cb8f061885b22e4c30451d92d709
Entitd <amaluteshev92@gmail.com>
2026-07-16 22:54:24 +0300

    clear metrics

--- app/Http/Controllers/Api/MetricsController.php
+++ app/Http/Controllers/Api/MetricsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class MetricsController extends Controller
 {
